b8f59947025a508bfb4e4a3b08f68ee54598280f
YurasovDV <yurasovdv@gmail.com>
2020-02-22 19:44:56 +0300

    added files

---
+++ BusinessMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace WordCounter.Common
+{
+    [JsonObject(Title = "business_message")]
+    public class BusinessMessage
+    {
+        [JsonProperty(PropertyName = "content")]
+        public string Content { get; set; }
+
+        [JsonProperty(PropertyName = "correlation_id")]
+        public Guid CorrelationId { get; set; }
+    }
+}
---
+++ Constants.cs
@@ -0,0 +1,14 @@
+﻿namespace WordCounter.Common
+{
+    public class Constants
+    {
+        public const string RabbitMqHost = "RabbitMqHost";
+        public const string RabbitMqPort = "RabbitMqPort";
+        public const string RabbitMqUser = "RabbitMqUser";
+        public const string RabbitMqPass = "RabbitMqPass";
+
+        public const string ArticlesExchange = "articles";
+        public const string RoutingKey = "";
+        public const string HelloWorldMessage = "060ca0f6-f111-4e45-be12-7d62832bf328";
+    }
+}
