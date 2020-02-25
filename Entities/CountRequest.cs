cc76312e80ef6f9dd5b816012b5dcf253f924767
YurasovDV <yurasovdv@gmail.com>
2020-02-25 21:53:31 +0300

    added common entities for db

---
+++ Entities/CountRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WordCounter.Common
+{
+    [Table("count_requests")]
+    public class CountRequest
+    {
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("correlation_id")]
+        public Guid CorrelationId { get; set; }
+
+        [Column("content")]
+        public string Content { get; set; }
+    }
+}
